d49988b49b77931e5e50a9135afcf62559576859
masahirotamaki <masahiro.tamaki53@gmail.com>
2020-01-31 00:26:32 +0900

    ログイン機能の追加と修正
    ビューの修正/記事の投稿、編集、削除の認証必須/記事作成時に著者のIDを保存

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,98 +7,107 @@ use App\Post; //Postモデルをインポート
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    // 一覧表示
-    public function index()
-    {
-      // 投稿日の新しい順で記事を5件ずつ一覧表示
-      $posts = Post::latest()->paginate(5); //latest(), oldest() デフォルトでcreated_atカラムによりソートされます。
-      return view('posts.index', ['posts' => $posts]);
-    }
+  /**
+   * 各アクションの前に実行させるミドルウェア
+   */
+  public function __construct()
+  {
+    $this->middleware('auth')->except(['index', 'show']);
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    // 新規投稿用フォームへ移動
-    public function create()
-    {
-        return view('posts.create');
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  // 一覧表示
+  public function index()
+  {
+    // 投稿日の新しい順で記事を5件ずつ一覧表示
+    $posts = Post::latest()->paginate(5); //latest(), oldest() デフォルトでcreated_atカラムによりソートされます。
+    return view('posts.index', ['posts' => $posts]);
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    // 実際の投稿処理
-    // 完了後、投稿した記事のページへ移動
-    public function store(Request $request)
-    {
-        $post = new Post; //新しいインスタンスを作成
-        $post->title = $request->title; //それぞれの値を保存して
-        $post->body = $request->body;
-        $post->save(); //DBに保存
-        return redirect('posts/'.$post->id); //記事詳細ページへ移動
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  // 新規投稿用フォームへ移動
+  public function create()
+  {
+    return view('posts.create');
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    // １件表示
-    public function show(Post $post)
-    {
-        return view('posts.show', ['post' => $post]);
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  // 実際の投稿処理
+  // 完了後、投稿した記事のページへ移動
+  public function store(Request $request)
+  {
+    $post = new Post; //新しいインスタンスを作成
+    $post->title = $request->title; //それぞれの値を保存して
+    $post->body = $request->body;
+    $post->user_id = $request->user()->id;
+    $post->save(); //DBに保存
+    return redirect('posts/' . $post->id); //記事詳細ページへ移動
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    // 更新用フォームへ移動
-    public function edit(Post $post)
-    {
-      return view('posts.edit', ['post' => $post]);
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  // １件表示
+  public function show(Post $post)
+  {
+    return view('posts.show', ['post' => $post]);
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    // 実際の更新処理
-    // 完了後、更新した記事のページへ移動
-    public function update(Request $request, Post $post)
-    {
-        $post->title = $request->title;
-        $post->body = $request->body;
-        $post->save();
-        return redirect('posts/'.$post->id);
-    }
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  // 更新用フォームへ移動
+  public function edit(Post $post)
+  {
+    return view('posts.edit', ['post' => $post]);
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    // 一件削除
-    public function destroy(Post $post)
-    {
-        $post->delete();
-        return redirect('posts');
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  // 実際の更新処理
+  // 完了後、更新した記事のページへ移動
+  public function update(Request $request, Post $post)
+  {
+    $post->title = $request->title;
+    $post->body = $request->body;
+    $post->save();
+    return redirect('posts/' . $post->id);
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  // 一件削除
+  public function destroy(Post $post)
+  {
+    $post->delete();
+    return redirect('posts');
+  }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,101 +8,109 @@ use App\User;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    //ユーザーの一覧表示
-    public function index()
-    {
-        $users = User::paginate(5);  //すべてのユーザーを5件ずつ表示
-        return view('users.index', ['users' => $users]);
-        //第一引数には"遷移先のbladeファイル名", 第二引数は"['渡す先での変数名' => 今回渡す変数]"
-    }
+  /**
+   * 各アクションの前に実行させるミドルウェア
+   */
+  public function __construct()
+  {
+    $this->middleware('auth')->except(['index', 'show']);
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    // 追加用のフォームへ移動
-    public function create()
-    {
-        return view('users.create');
-    }
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  //ユーザーの一覧表示
+  public function index()
+  {
+    $users = User::paginate(5);  //すべてのユーザーを5件ずつ表示
+    return view('users.index', ['users' => $users]);
+    //第一引数には"遷移先のbladeファイル名", 第二引数は"['渡す先での変数名' => 今回渡す変数]"
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    // 実際の追加処理
-    // 完了後、作成したユーザーのページへ移動
-    public function store(Request $request)
-    {
-        $user = new User;  //新しいインスタンスを作成
-        $user->name = $request->name;  //それぞれの値を保存して
-        $user->email = $request->email;
-        $user->password = $request->password;
-        $user->save();  //DBに保存
-        return redirect('users/'.$user-id);  //ユーザー詳細ページへ移動
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  // 追加用のフォームへ移動
+  public function create()
+  {
+    return view('users.create');
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    //ユーザー1件詳細表示
-    public function show(User $user)
-    //「モデル結合ルート」を利用して簡潔に記述するため、引数は$idではなくUser $userとする。
-    {
-        // そのユーザーが投稿した記事のうち、最新5件を取得
-        $user->posts = $user->posts()->paginate(5);
-        return view('users.show',['user' => $user]);
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  // 実際の追加処理
+  // 完了後、作成したユーザーのページへ移動
+  public function store(Request $request)
+  {
+    $user = new User;  //新しいインスタンスを作成
+    $user->name = $request->name;  //それぞれの値を保存して
+    $user->email = $request->email;
+    $user->password = $request->password;
+    $user->save();  //DBに保存
+    return redirect('users/' . $user - id);  //ユーザー詳細ページへ移動
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    // 更新用フォームへ移動
-    public function edit(User $user)
-    {
-        return view('users.edit', ['user' => $user]);
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  //ユーザー1件詳細表示
+  public function show(User $user)
+  //「モデル結合ルート」を利用して簡潔に記述するため、引数は$idではなくUser $userとする。
+  {
+    // そのユーザーが投稿した記事のうち、最新5件を取得
+    $user->posts = $user->posts()->paginate(5);
+    return view('users.show', ['user' => $user]);
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    // 実際の更新処理
-    // 完了後、更新したユーザーのページへ移動
-    public function update(Request $request, User $user)
-    {
-        $user->name = $request->name;
-        $user->save();
-        return redirect('users/'.$user->id);
-    }
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  // 更新用フォームへ移動
+  public function edit(User $user)
+  {
+    return view('users.edit', ['user' => $user]);
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    // 一件削除
-    public function destroy(User $user)
-    {
-        $user->delete();
-        return redirect('users');
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  // 実際の更新処理
+  // 完了後、更新したユーザーのページへ移動
+  public function update(Request $request, User $user)
+  {
+    $user->name = $request->name;
+    $user->save();
+    return redirect('users/' . $user->id);
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  // 一件削除
+  public function destroy(User $user)
+  {
+    $user->delete();
+    return redirect('users');
+  }
 }
